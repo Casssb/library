faa737ae18dd51885fd844781fd6579a4199bc9f
Joe <caspar0912@googlemail.com>
2022-08-10 15:14:56 +0100

    added modal button logic

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 const openBookModal = document.querySelector('#open-book-modal');
 const darkmode = document.querySelector('#darkmode');
 const modal = document.querySelector('#modal');
+const closeBookModal = document.querySelector('#book-form-close')
 
 
 openBookModal.addEventListener('click', () => {
     modal.style.display = 'flex';
+});
+
+closeBookModal.addEventListener('click', () => {
+    modal.style.display = 'none';
 })
 
 window.onclick = function(event) {
