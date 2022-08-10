2e9ae90968475c5fff7622838621779aa7da91ac
Joe <caspar0912@googlemail.com>
2022-08-10 20:51:24 +0100

    added form nodes & updated basic functions

--- main.js
+++ main.js
@@ -1,8 +1,33 @@
 const openBookModal = document.querySelector('#open-book-modal');
 const darkmode = document.querySelector('#darkmode');
 const modal = document.querySelector('#modal');
-const closeBookModal = document.querySelector('#book-form-close')
+const closeBookModal = document.querySelector('#book-form-close');
 
+/* Book form nodes*/
+const bookTitle = document.querySelector('#book-form-title');
+const bookAuthor = document.querySelector('#book-form-author');
+const bookPages = document.querySelector('#book-form-pages');
+const bookDescription = document.querySelector('#book-form-description');
+const bookRating = document.querySelectorAll(`.book-rating:checked`);
+const bookRead = document.querySelector('#book-form-read');
+const bookSubmit = document.querySelector('#book-form-submit');
+
+const booklist = []
+
+function Book(title, author, pages, description, rating, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.description = description;
+  this.rating = rating;
+  this.read = read;
+}
+
+function addToBooklist() {
+
+}
+
+bookSubmit.addEventListener('submit', addToBooklist())
 
 openBookModal.addEventListener('click', () => {
     modal.style.display = 'flex';
