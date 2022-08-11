bd7d2d5dbda38afd77467cb3ddb1380243c5769d
Joe <caspar0912@googlemail.com>
2022-08-11 22:03:14 +0100

    add logic for submitting form & updating array JS

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const modal = document.querySelector('#modal');
 const closeBookModal = document.querySelector('#book-form-close');
 
 /* Book form nodes*/
+const bookForm = document.querySelector('#book-form')
 const bookTitle = document.querySelector('#book-form-title');
 const bookAuthor = document.querySelector('#book-form-author');
 const bookPages = document.querySelector('#book-form-pages');
@@ -12,7 +13,7 @@ const bookRating = document.querySelectorAll(`.book-rating:checked`);
 const bookRead = document.querySelector('#book-form-read');
 const bookSubmit = document.querySelector('#book-form-submit');
 
-const booklist = []
+const booklist = [];
 
 function Book(title, author, pages, description, rating, read) {
   this.title = title;
@@ -24,21 +25,41 @@ function Book(title, author, pages, description, rating, read) {
 }
 
 function addToBooklist() {
+  let book = new Book(
+    bookTitle.value,
+    bookAuthor.value,
+    bookPages.value,
+    bookDescription.value,
+    bookRead.checked
+  );
+  booklist.push(book)
+  console.log('event happened')
+}
+
+function clearForm() {
 
 }
 
-bookSubmit.addEventListener('submit', addToBooklist())
+function appendBooksToDom() {
+
+}
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addToBooklist()
+  modal.style.display = 'none'
+});
 
 openBookModal.addEventListener('click', () => {
-    modal.style.display = 'flex';
+  modal.style.display = 'flex';
 });
 
 closeBookModal.addEventListener('click', () => {
-    modal.style.display = 'none';
-})
+  modal.style.display = 'none';
+});
 
-window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
   }
+};
